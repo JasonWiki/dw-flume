733c1743afb33e1f36ce26b49f34fe716e5e8ea5
jason <jason@angejia.com>
2017-01-16 11:55:34 +0800

    first

---
+++ src/main/java/com/angejia/dw/flume/App.java
@@ -0,0 +1,13 @@
+package com.angejia.dw.flume;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
